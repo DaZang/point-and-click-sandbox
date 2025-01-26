611f5bfb102e8684365aa51aed2a5e0076d45286
David Zang <david.zang@viadee.de>
2025-01-26 21:47:23 +0100

    ✨ add dialogue_balloon and simple dialogue example

--- entities/characters/tower_guard.gd
+++ entities/characters/tower_guard.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@export var dialogue_resource: DialogueResource
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("left_mouse_button"):
-		print("yeah")
+		DialogueManager.show_dialogue_balloon(dialogue_resource, "start")
